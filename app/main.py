25a7ba0ff466804ccbabede992ae203b1d76a9fb
Yuna Park <yunaz0402@gmail.com>
2025-02-17 21:17:08 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,10 +7,20 @@ from fastapi import FastAPI, Depends, HTTPException, status
 # from passlib.context import CryptContext
 # from datetime import datetime,  timedelta, timezone
 # from typing import Optional, List
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 # from typing import Annotated
 
+
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
         
 @app.get("/")
 def read_root():
