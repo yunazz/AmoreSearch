63eb5c525162e0624c06b98375df102992b849f1
Yuna Park <yunaz0402@gmail.com>
2025-02-17 23:47:20 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,14 @@
-from app.routes import router
-
 from fastapi import FastAPI, Depends, HTTPException, status
 # from jose import JWTError, jwt
 # from passlib.context import CryptContext
 # from datetime import datetime,  timedelta, timezone
 # from typing import Optional, List
-from fastapi.middleware.cors import CORSMiddleware
 # from typing import Annotated
 
 
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-        
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
