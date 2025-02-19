45ff018cc582a4c825604d7399ad678f78c2682d
Yuna Park <yunaz0402@gmail.com>
2025-02-20 00:59:36 +0900

    .

---
+++ frontend/pages/news.vue
@@ -0,0 +1,16 @@
+<script setup>
+// const router = useRouter();
+const member = ref({
+  name: "홍길동",
+  position: "사원",
+  department: "HR",
+});
+const formData = ref({});
+</script>
+<template>
+  <div id="News" class="page">
+    <div class="contents"></div>
+  </div>
+</template>
+
+<style scoped></style>
