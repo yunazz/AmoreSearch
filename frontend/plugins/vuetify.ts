73f7e124fd5a998835a1f43e8b66763061c0095e
Yuna Park <yunaz0402@gmail.com>
2025-02-20 01:20:55 +0900

    .

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,5 +1,5 @@
 import { createVuetify } from 'vuetify';
-import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import * as components from 'vuetify/components'
 import 'vuetify/styles';
 
