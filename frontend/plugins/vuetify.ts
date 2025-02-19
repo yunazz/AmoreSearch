8ce81b82558dada077eead629490f2dd84a2cf76
Yuna Park <yunaz0402@gmail.com>
2025-02-20 01:33:45 +0900

    Update vuetify.ts

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,17 +1,11 @@
 import { createVuetify } from 'vuetify';
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css';
+
 import * as components from 'vuetify/components'
 import 'vuetify/styles';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    icons: {
-      defaultSet: 'mdi',
-      aliases,
-      sets: {
-        mdi,
-      },
-    },
     components,
     theme: {
       themes: {
