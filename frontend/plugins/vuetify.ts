6fc26d68514d0dfc1941bebf07cfd0d4e467009f
Yuna Park <yunaz0402@gmail.com>
2025-02-20 01:14:50 +0900

    .

--- frontend/plugins/vuetify.ts
+++ frontend/plugins/vuetify.ts
@@ -1,10 +1,17 @@
 import { createVuetify } from 'vuetify';
-import '@mdi/font/css/materialdesignicons.css';
-import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
 import * as components from 'vuetify/components'
+import 'vuetify/styles';
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    icons: {
+      defaultSet: 'mdi',
+      aliases,
+      sets: {
+        mdi,
+      },
+    },
     components,
     theme: {
       themes: {
