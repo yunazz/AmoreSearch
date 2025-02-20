0b790373af09e0232ede0705cea7e2f4d5ac5f90
Yuna Park <yunaz0402@gmail.com>
2025-02-20 12:45:10 +0900

    Update ai_search.vue

--- frontend/pages/ai_search.vue
+++ frontend/pages/ai_search.vue
@@ -19,18 +19,63 @@ const formData = ref({});
         <br />
         무엇을 도와드릴까요?
       </h2>
-      <div class="input--search mt-3">
-        <v-text-field
-          prepend-inner-icon="mdi-magnify"
-          variant
-          hide-details
-          single-line
-          density="compact"
-        />
+      <div class="input--search mt-5">
+        <v-icon icon="mdi-magnify" />
+        <input type="text" />
       </div>
-      <div>
-        <div class="card_outlined"></div>
-      </div>
+    </div>
+    <v-divider class="mt-9" />
+    <article class="mt-7">
+      <v-sheet class="group_card" style="">
+        <v-slide-group v-model="model" class="pa-2" show-arrows>
+          <v-slide-group-item v-for="n in 8" :key="n">
+            <div class="group_card_item">
+              <!-- <v-card :class="['ma-4']" height="250" width="340" @click="toggle"> -->
+              <div class="d-flex fill-height align-center justify-center">
+                item
+              </div>
+              <!-- </v-card> -->
+            </div>
+          </v-slide-group-item>
+        </v-slide-group>
+      </v-sheet>
+    </article>
+    <div class="container_inner grid-cols-2">
+      <article class="mt-2 px-3">
+        <v-card class="mx-auto" elevation="0">
+          <v-list :items="items" lines="two" item-props>
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <NuxtImg src="img/icon/story.svg" width="30" />
+              </v-avatar>
+            </template>
+            <template v-slot:subtitle="{ subtitle }">
+              <div v-html="subtitle"></div>
+            </template>
+            <template v-slot:append>
+              <v-btn
+                color="grey-lighten-2"
+                icon="mdi-star"
+                variant="text"
+              ></v-btn>
+            </template>
+          </v-list>
+        </v-card>
+      </article>
+      <article class="mt-2 px-3">
+        <v-card class="mx-auto" elevation="0">
+          <v-list :items="items1" lines="two" item-props>
+            <template v-slot:prepend>
+              <v-avatar color="grey-lighten-1">
+                <v-icon color="white">mdi-folder</v-icon>
+              </v-avatar>
+            </template>
+            <template v-slot:subtitle="{ subtitle }">
+              <div v-html="subtitle"></div>
+            </template>
+          </v-list>
+        </v-card>
+      </article>
     </div>
   </div>
 </template>
