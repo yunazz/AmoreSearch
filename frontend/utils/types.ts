4add4d15ca9ab4c482dd7dd98610bd4f9028a7ac
Yuna Park <yunaz0402@gmail.com>
2025-02-23 07:00:39 +0900

    Update types.ts

--- frontend/utils/types.ts
+++ frontend/utils/types.ts
@@ -1,19 +1,19 @@
-type RoleTypes = '시스템관리자' | '관리자' | '일반' | '';
+type RoleTypes = 3 | 2 | 1 | 0; // '시스템관리자' | '관리자' | '일반' | '';
 
 interface ResponseOutput {
-    code: number;
-    msg: String;
-    result: any;
-    total_rows?: any;
+  code: number;
+  msg: String;
+  result: any;
+  total_rows?: any;
 }
 
 interface JwtPayloadOptions {
-    member_id: number;
-    emp_no: string;
-    position: string;
-    department: string;
-    name: string;
-    role: RoleTypes;
+  member_id: number;
+  emp_no: string;
+  position: string;
+  department: string;
+  name: string;
+  role: RoleTypes;
 }
 
 export interface MemberOptions extends JwtPayloadOptions {}
