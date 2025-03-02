40381e1f858b25cfeb530afe25772010f836ce71
Yuna Park <yunaz0402@gmail.com>
2025-03-02 14:59:20 +0900

    Update auth.ts

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -2,8 +2,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const member = useMember();
 
   if (member.value.role < 2 || member.value.department !== "HR팀") {
-    console.log(member.value.role);
-
     return navigateTo("/dashboard");
   }
 });
